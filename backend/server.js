f0f6293fd829b0cf07cac052e63e975aaf67b53a
youssef09baka <youssef09baka@gmail.com>
2026-03-30 16:29:03 +0100

    vérification que module et le port ne push pas et .env marche

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
-require("dotenv").config();
-
 const app = express();
 
 // Mongo
@@ -18,7 +16,7 @@ app.get("/", (req, res) => {
 });
 
 // PORT depuis .env
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`🚀 serveur sur http://localhost:${PORT}`);
