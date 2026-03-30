65696833ac846f38d4732d45d2fefc4496dbb438
youssef09baka <youssef09baka@gmail.com>
2026-03-30 15:55:44 +0100

    j'ai connecter mon back avec mangos

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+// 🔗 MongoDB
+mongoose.connect("mongodb+srv://Jusef123:QfDu3lcocWZ5rXqG@cluster0.cz9qh0f.mongodb.net/test")
+.then(() => console.log("✅ MongoDB connecté"))
+.catch(err => console.log("❌ erreur :", err.message));
+
+// route test
+app.get("/", (req, res) => {
+    res.send("API fonctionne 🚀");
+});
+
+app.listen(5000, () => {
+    console.log("🚀 serveur sur http://localhost:5000");
+});
